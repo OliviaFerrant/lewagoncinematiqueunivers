d22aea74574fc2b1a6d1c4bbe8d7dae032f0984b
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-25 19:04:01 +0900

    Title style & img hover added

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,3 +1,28 @@
 class PowersController < ApplicationController
+  before_action :set_character, only: [:new, :create]
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.new(power_params)
+    @power.character = @character
+    if @power.save
+      redirect_to character_path(@character)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_character
+    @character = Character.find(params[:character_id])
+  end
+
+  def power_params
+    params.require(:power).permit(:power)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "characters#index"
   resources :characters do
-    resources :power, only: [:new, :create, :edit, :update]
+    resources :powers, only: [:new, :create, :edit, :update]
   end
-  resources :power, only: [:destroy]
+  resources :powers, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
